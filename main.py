33757848889a630d15644b2bf7754fdeeeb56621
MatteoAndriolo <matteo.andriolo.2@studenti.unipd.it>
2023-07-22 16:54:49 +0200

    working main

--- main.py
+++ main.py
@@ -1,133 +1,92 @@
-# Path to data Files, path to external tools and hyperparameters
-import os
-from multiprocessing import cpu_count
+import argparse
+import datetime
+import sys
 
-DEBUG = False
-MTHREAD = cpu_count() * 2
-KFOLDS = 2
-EPOCHS = 2
-BATCH_SIZE = 512 
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-os.chdir(ROOT_DIR)
-PATH_DSSP = os.path.join(ROOT_DIR, "src/mkdssp")  # Path to DSSP executable
-PATH_FEATURES_RING = os.path.join(ROOT_DIR, "data/features_ring")
-PATH_PDB = os.path.join(ROOT_DIR, "data/pdb")
-# PATH_NEW_FEATURE_RING = os.path.join(ROOT_DIR, "data/feature_ring_new/")
-PATH_DSSP_TSV = os.path.join(ROOT_DIR, "data/tsv")
-
-RAMA_FILE = "data/ramachandran.dat"
-ATCHLEY_FILE = "data/atchley.tsv"
-DISTANCE_THRESHOLD = 3.5
-SEQUENCE_SEPARATION = 3
-RAMA_SS_RANGES = [
-    [-180, -180, 80, 60, "E", "blue"],
-    [-180, 50, 80, 130, "E", "blue"],
-    [-100, -180, 100, 60, "P", "green"],
-    [-100, 50, 100, 130, "P", "green"],
-    [-180, -120, 180, 170, "H", "red"],
-    [0, -180, 180, 360, "L", "yellow"],
-]
-
-INTERACTION_TYPES = [
-    "HBOND",
-    "VDW",
-    "PIPSTACK",
-    "IONIC",
-    "SSBOND",
-    "PICATION",
-    "Unclassified",
-]
+import configuration as conf
+import src.neural_networks as neural_networks
+from src.manage_data import build_index, check_resi, prepare_data
+from src.normalization import normalization_df
 
 
-COLUMS_SMALL = [
-    "pdb_id",
-    "s_ch",
-    "s_resi",
-    "s_ins",
-    "s_resn",
-    "s_ss8",
-    "s_rsa",
-    "s_up",
-    "s_down",
-    "s_phi",
-    "s_psi",
-    "s_ss3",
-    "s_a1",
-    "s_a2",
-    "s_a3",
-    "s_a4",
-    "s_a5",
-    "t_ch",
-    "t_resi",
-    "t_ins",
-    "t_resn",
-    "t_ss8",
-    "t_rsa",
-    "t_up",
-    "t_down",
-    "t_phi",
-    "t_psi",
-    "t_ss3",
-    "t_a1",
-    "t_a2",
-    "t_a3",
-    "t_a4",
-    "t_a5",
-    "Interaction",
-]
+def parser():
+    parser = argparse.ArgumentParser(
+        description="prediction of the interactions for RING software"
+    )
+    parser.add_argument("-m", "--model", help="model name", required=False)
+    parser.add_argument(
+        "-n", "--normalization", help="normalization name", required=False
+    )
+    # parser.add_argument(
+    #     "-d", "--manipulation", help="dataset manipulation type", required=False
+    # )
 
-COLUMNS_BIG = [
-    "pdb_id",
-    "s_ch",
-    "s_resi",
-    "s_ins",
-    "s_resn",
-    "s_ss8",
-    "s_rsa",
-    "s_nh_relidix",
-    "s_nh_energy",
-    "s_o_relidx",
-    "s_o_energy",
-    "s_nh2_relidx",
-    "s_nh2_energy",
-    "s_o2_relidx",
-    "s_o2_energy",
-    "s_up",
-    "s_down",
-    "s_phi",
-    "s_psi",
-    "s_ss3",
-    "s_a1",
-    "s_a2",
-    "s_a3",
-    "s_a4",
-    "s_a5",
-    "t_ch",
-    "t_resi",
-    "t_ins",
-    "t_resn",
-    "t_ss8",
-    "t_rsa",
-    "t_nh_relidix",
-    "t_nh_energy",
-    "t_o_relidx",
-    "t_o_energy",
-    "t_nh2_relidx",
-    "t_nh2_energy",
-    "t_o2_relidx",
-    "t_o2_energy",
-    "t_up",
-    "t_down",
-    "t_phi",
-    "t_psi",
-    "t_ss3",
-    "t_a1",
-    "t_a2",
-    "t_a3",
-    "t_a4",
-    "t_a5",
-    "Interaction",
-]
+    args = parser.parse_args()
 
-COLUMN_DSSP = COLUMNS_BIG[:-1]
+    if len(sys.argv) <= 1:
+        return []
+
+    error = []
+    if not args.model:
+        error.append("Model name is required.")
+
+    if not args.normalization:
+        error.append("Normalization name is required.")
+
+    # if not args.manipulation:
+    #     error.append("Dataset manipulation type is required.")
+
+    if error:
+        parser.print_help()
+        raise SystemExit(f"\n{' '.join(error)}")
+
+    return args
+
+
+def main(df, model_name, normalization_mode, f):
+    print(model_name, normalization_mode)
+    df = normalization_df(df, normalization_mode)
+    neural_networks.kfold_train(df, model_name, f)
+
+
+if __name__ == "__main__":
+    args = parser()
+    now = datetime.datetime.now().strftime(r"%m-%d_%H:%M")
+    out = f"./results{now}.csv"
+
+    conf.DEBUG = True
+    # conf.MTHREAD=1
+
+    index = build_index(
+        path_pdb=conf.PATH_PDB,
+        path_ring=conf.PATH_FEATURES_RING,
+        path_tsv=conf.PATH_DSSP_TSV,
+        generate_new=False,
+        update_dssp=False,
+    )
+
+    df = prepare_data(index)
+    # df.to_csv("data.tsv")
+
+    with open(out, "w") as f:
+        f.write(
+            f"model_name\taverage_accuracy\taverage_f1\taverage_precision\taverage_recall\n"
+        )
+        if len(sys.argv) > 1:
+            main(df, args.model, args.normalization, f)
+            # scale = args.normalization
+            # model = args.model
+            # f.write(f"### {scale}\n")
+            # main(df, scale, model, f)
+        else:
+            # scales = ["StandardScaler", "MinMaxScaler"]
+            scales = ["MinMaxScaler"]
+            models = ["model_1", "model_2", "model_3"]
+
+            for scale in scales:
+                f.write(f"\n### {scale}\n")
+                df_norm = normalization_df(df, scale)
+                for model_name in models:
+                    print(f"Start training {model_name}")
+
+                    # neural_networks.kfold_train(df_norm,model_name,f)
+                    neural_networks.gridsearch(df_norm, model_name, f)
